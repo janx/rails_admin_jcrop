5a2405a9c23eea60cb9480d74bbfddbd53c73722
Jan Xie <jan.h.xie@gmail.com>
2012-08-17 11:00:06 +0800

    Revert "get geometry with rmagick"
    This reverts commit b9e84c37c277c589045129ed8ed4c85deade161c.

--- lib/rails_admin_jcrop/detector.rb
+++ lib/rails_admin_jcrop/detector.rb
@@ -4,14 +4,7 @@ module RailsAdminJcrop
       def upload_plugin
         return 'CarrierWave' if defined?(CarrierWave) && CarrierWave.respond_to?(:configure)
         return 'Paperclip' if defined?(Paperclip) && Paperclip.respond_to?(:options)
-        Rails.logger.warn "[RailsAdminJcrop] Cannot find available upload plugin. (CarrierWave or Paperclip)"
-        nil
-      end
-
-      def image_plugin
-        return 'MiniMagick' if defined?(MiniMagick)
-        return 'RMagick' if defined?(Magick)
-        Rails.logger.warn "[RailsAdminJcrop] Cannot find available image plugin. (MiniMagick or RMagick)"
+        Rails.logger.warn "[RailsAdminJcrop] Cannot find available upload plugin. (carrierwave or paperclip)"
         nil
       end
     end
