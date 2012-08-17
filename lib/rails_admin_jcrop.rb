89f920d1481d075fc9954e31b4c5bbf19b87866d
Jan Xie <jan.h.xie@gmail.com>
2012-08-17 12:17:42 +0800

    refactor carrierwave to carrier_wave

--- lib/rails_admin_jcrop.rb
+++ lib/rails_admin_jcrop.rb
@@ -1,7 +1,10 @@
 require "rails_admin_jcrop/detector"
 require "rails_admin_jcrop/engine"
+
 require 'rails_admin_jcrop/orm/active_record' if defined?(::ActiveRecord)
 require 'rails_admin_jcrop/orm/mongoid' if defined?(::Mongoid)
-require 'rails_admin_jcrop/extensions/carrierwave' if defined?(::CarrierWave)
+
+require 'rails_admin_jcrop/extensions/carrier_wave' if defined?(::CarrierWave)
 require 'rails_admin_jcrop/extensions/paperclip' if defined?(::Paperclip)
+
 require 'rails_admin_jcrop/extensions/rails_admin'
