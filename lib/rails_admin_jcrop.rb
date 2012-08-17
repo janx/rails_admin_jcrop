c717037e79408d9dd871844e3d0a51eba8342d7f
Jan Xie <jan.h.xie@gmail.com>
2012-08-17 12:34:31 +0800

    refactor rails_admin extension

--- lib/rails_admin_jcrop.rb
+++ lib/rails_admin_jcrop.rb
@@ -6,4 +6,4 @@ require 'rails_admin_jcrop/orm/mongoid' if defined?(::Mongoid)
 require 'rails_admin_jcrop/asset_engine/carrier_wave' if defined?(::CarrierWave)
 require 'rails_admin_jcrop/asset_engine/paperclip' if defined?(::Paperclip)
 
-require 'rails_admin_jcrop/extensions/rails_admin'
+require 'rails_admin_jcrop/rails_admin_extension'
