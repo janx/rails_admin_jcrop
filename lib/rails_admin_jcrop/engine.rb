07ce0b7f366c86557cec1a09f86aa393e57d8eb4
Jan Xie <jan.h.xie@gmail.com>
2013-03-15 10:09:36 +0800

    Fix crop for new RMagick

--- lib/rails_admin_jcrop/engine.rb
+++ lib/rails_admin_jcrop/engine.rb
@@ -2,6 +2,8 @@ module RailsAdminJcrop
   class Engine < ::Rails::Engine
     isolate_namespace RailsAdmin
 
+    config.autoload_paths << File.expand_path('../..', __FILE__)
+
     initializer "RailsAdminJcrop precompile hook" do |app|
       app.config.assets.precompile += ['rails_admin/ra.jcrop.js', 'rails_admin/jquery.Jcrop.js', 'rails_admin/jquery.Jcrop.css']
     end
