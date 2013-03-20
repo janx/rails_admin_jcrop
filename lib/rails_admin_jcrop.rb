4b5d628dba7ba6063cf05db9bfb552a33ace8004
Jan Xie <jan.h.xie@gmail.com>
2013-03-20 13:37:54 +0800

    choose image processor at runtime, ditch my stupid concern on efficiency

--- lib/rails_admin_jcrop.rb
+++ lib/rails_admin_jcrop.rb
@@ -1,4 +1,5 @@
 require "rails_admin_jcrop/engine"
+require 'rails_admin_jcrop/image_helper'
 require 'rails_admin_jcrop/orm'
 require 'rails_admin_jcrop/asset_engine'
 require 'rails_admin_jcrop/rails_admin'
--- lib/rails_admin_jcrop/image_helper.rb
+++ lib/rails_admin_jcrop/image_helper.rb
@@ -1,2 +1,21 @@
-filename = defined?(::Magick) ? 'rmagick' : 'minimagick'
-require "rails_admin_jcrop/image_helper/#{filename}"
+module RailsAdminJcrop
+  module ImageHelper
+
+    class <<self
+      def crop(*args)
+        processor = defined?(::Magick) ? 'rmagick' : 'minimagick'
+        send("#{processor}_crop", *args)
+      end
+
+      def minimagick_crop(img, w, h, x, y)
+        geometry = "#{w}x#{h}+#{x}+#{y}"
+        img.crop geometry
+      end
+
+      def rmagick_crop(img, w, h, x, y)
+        img.crop! x.to_i, y.to_i, w.to_i, h.to_i
+      end
+    end
+
+  end
+end
