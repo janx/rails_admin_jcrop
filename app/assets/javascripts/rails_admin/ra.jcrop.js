e461884845e39c1ef976fa89e48504e2a070e11f
Salim Hbeiliny <salim@railslove.com>
2015-03-20 10:17:08 +0100

    js: more clear thumbnail image lookup

--- app/assets/javascripts/rails_admin/ra.jcrop.js
+++ app/assets/javascripts/rails_admin/ra.jcrop.js
@@ -5,7 +5,7 @@
       var widget = this;
       var dom_widget = widget.element;
 
-      var thumbnailLink = dom_widget.find('img[class^=img-]').parent();
+      var thumbnailLink = dom_widget.find('img.img-polaroid, img.img-thumbnail').first().parent(); // workaround for different bootstrap versions using a different class for thumbnails
       thumbnailLink.unbind().bind("click", function(e){
         widget._bindModalOpening(e, dom_widget.find('a.jcrop_handle').data('link'));
         return false;
